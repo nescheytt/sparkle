4bbde5eb0fc2e5348013617530bb6b1c7c17feb4
nescheytt <nahuel.scheytt@gmail.com>
2023-11-29 13:28:04 +0000

    feat: metadata

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,4 @@
+import { Metadata } from "next"
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 
@@ -9,4 +10,48 @@ export function absoluteUrl(path: string) {
   if (typeof window !== "undefined") return path
   if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}${path}`
   return `https://localhost:${process.env.PORT ?? 3000}${path}`
+}
+
+export function constructMetadata({
+  title = "Sparkle - Chat with your documents in seconds",
+  description = "Sparkle allows you to have conversations with any PDF document. Simply upload your file and start asking questions right away.",
+  image = "/thumbnail.png",
+  icons = "/favicon.icon",
+  noIndex = false,
+}: {
+  title?: string
+  description?: string
+  image?: string
+  icons?: string
+  noIndex?: boolean
+} = {}): Metadata {
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      images: [
+        {
+          url: image,
+        },
+      ],
+    },
+    twitter: {
+      card: "summary_large_image",
+      title,
+      description,
+      images: [image],
+      creator: "@nahueesch",
+    },
+    icons,
+    metadataBase: new URL("https://sparkle-six.vercel.app"),
+    themeColor: "#FFF",
+    ...(noIndex && {
+      robots: {
+        index: false,
+        follow: false,
+      },
+    }),
+  }
 }
